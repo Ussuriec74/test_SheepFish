31f26d4151bc2e12aa7ab70dffb70e143d92cfe1
Ussuriec74 <95906717+Ussuriec74@users.noreply.github.com>
2023-03-27 18:30:37 +0000

    Deploying to gh-pages from @ Ussuriec74/test_sheepfish@11144dc7a1504f2f2aba04d99b159de758f1dd44 🚀

---
+++ static/js/266.b0fd45a6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_sheepfish=self.webpackChunktest_sheepfish||[]).push([[266],{266:function(e,s,h){h.r(s);var n=h(184);s.default=function(){return(0,n.jsx)("main",{children:(0,n.jsx)("h1",{children:"Home"})})}}}]);
+//# sourceMappingURL=266.b0fd45a6.chunk.js.map
